edc3c52bae3e514e39fc5397322e217b4acc183b
koyaja <koyajawil@gmail.com>
2021-03-03 11:42:32 +0100

    add Security

---
+++ src/main/java/com/solidarit/demo/repositories/UserRepository.java
@@ -0,0 +1,15 @@
+package com.solidarit.demo.repositories;
+
+import com.solidarit.demo.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    @Query(" select u from User u " +
+            " where u.username = ?1")
+    Optional<User> findUserWithName(String username);
+
+}
---
+++ src/main/java/com/solidarit/demo/services/UserService.java
@@ -0,0 +1,36 @@
+package com.solidarit.demo.services;
+
+import com.solidarit.demo.entities.User;
+import com.solidarit.demo.repositories.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+
+@Service
+@Slf4j
+
+public class UserService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        Objects.requireNonNull(username);
+        User user = userRepository.findUserWithName(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return user;
+    }
+
+}
